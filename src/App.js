141ed992d2443c1badf200b9b7b56e5aac0e5bc1
JohnPedraza <jpedraza1123@gmail.com>
2023-02-04 01:36:31 -0500

    improve layout

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import AllFoodPage from "./pages/AllFood";
 import NewFoodPage from "./pages/NewFood";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllFoodPage />} />
         <Route path="/new-food" element={<NewFoodPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
