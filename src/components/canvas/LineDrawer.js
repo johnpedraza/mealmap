d51262285e3ee9625a7da53875ecc1042a089384
JohnPedraza <jpedraza1123@gmail.com>
2023-02-04 08:51:45 -0500

    add cmu map with test drawings

--- src/components/canvas/LineDrawer.js
+++ src/components/canvas/LineDrawer.js
@@ -7,12 +7,18 @@ const LineDrawer = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
     const image = new Image();
-    image.src = 'https://picsum.photos/800/600';
-    canvas.width = 800
-    canvas.height = 600
+    //image.src = 'https://picsum.photos/800/600';
+    image.src = require('../../assets/cmu_map_th23.png')
+    image.width = 1000
+    image.height = 450
+    canvas.width = 1000
+    canvas.height = 450
 
     image.onload = () => {
       context.drawImage(image, 0, 0);
+      context.fillRect(350, 200, 25, 25);
+      //context.clearRect(45, 45, 60, 60);
+      //context.strokeRect(50, 50, 50, 50);
       context.beginPath();
       context.moveTo(100, 100);
       context.lineTo(200, 200);
