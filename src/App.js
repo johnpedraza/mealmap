f9751b18c252b7add7d3ad22685be9a5d56e858e
JohnPedraza <jpedraza1123@gmail.com>
2023-02-04 00:07:20 -0500

    rendering lists

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import AllFoodPage from "./pages/AllFood";
 import NewFoodPage from "./pages/NewFood";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllFoodPage />} />
         <Route path="/new-food" element={<NewFoodPage />} />
