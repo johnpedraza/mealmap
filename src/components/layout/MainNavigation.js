4cec2b04947d5d8d29f40d7aa4f6691096aa6166
JohnPedraza <jpedraza1123@gmail.com>
2023-02-04 03:22:34 -0500

    use context for more stuff

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,13 @@
+import { useContext } from "react"
+
 import { Link } from "react-router-dom"
 import classes from "./MainNavigation.module.css"
 
+import FavoritesContext from "../../store/favorites-context"
+
 function MainNavigation () {
+    const favoritesCtx = useContext(FavoritesContext)
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>MealMap</div>
@@ -14,7 +20,10 @@ function MainNavigation () {
                         <Link to='/new-food'>Add New Food</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>Favorites</Link>
+                        <Link to='/favorites'>
+                            Favorites
+                            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
